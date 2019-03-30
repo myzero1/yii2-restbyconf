b57a065d53c96c307f39d9222fb4dfd656460582
myzero1 <myzero1@sina.com>
2019-03-30 15:00:07 +0800

    add deleted

---
+++ src/components/rest/DeleteProcessing.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @link https://github.com/myzero1
+ * @copyright Copyright (c) 2019- My zero one
+ * @license https://github.com/myzero1/yii2-restbyconf/blob/master/LICENSE
+ */
+
+namespace myzero1\restbyconf\components\rest;
+
+/**
+ * ViewProcessing implements the API endpoint for creating a new model from the given data.
+ *
+ * For more details and usage information on CreateAction, see the [guide article](https://github.com/myzero1/yii2-restbyconf).
+ *
+ * @author Myzero1 <myzero1@sina.com>
+ * @since 0.0
+ */
+interface DeleteProcessing
+{
+    /**
+     * @param mixed $id
+     * @return array date will return to create action.
+     */
+    public function processing($id);
+
+    /**
+     * @return array
+     */
+    public function completeResult();
+
+    /**
+     * @param mixed $id
+     * @return Model the loaded model
+     */
+    public function findModel($id);
+}
--- src/models/Demo.sql
+++ src/models/Demo.sql
@@ -4,5 +4,6 @@ CREATE TABLE `sj_demo` (
   `description` varchar(255) NOT NULL,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
+  `is_del` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0未删除，非0为删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4
