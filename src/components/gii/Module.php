f3918a66f83c06c589e1764bf87b7bd545130be7
myzero1 <myzero1@sina.com>
2019-03-31 08:43:37 +0800

    holdon

--- src/components/gii/Module.php
+++ src/components/gii/Module.php
@@ -166,6 +166,7 @@ class Module extends \yii\base\Module implements BootstrapInterface
             'form' => ['class' => 'yii\gii\generators\form\Generator'],
             'module' => ['class' => 'yii\gii\generators\module\Generator'],
             'extension' => ['class' => 'yii\gii\generators\extension\Generator'],
+            'rest' => ['class' => 'myzero1\restbyconf\components\gii\generators\rest\Generator'],
         ];
     }
 
