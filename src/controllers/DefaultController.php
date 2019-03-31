bfaa7b151aed94e0aa395d8a52c48dfe977c51c4
myzero1 <myzero1@sina.com>
2019-03-31 14:03:41 +0800

    add swagger

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace myzero1\restbyconf\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Default controller for the `test` module
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionSwagger()
+    {
+        return $this->render('swagger');
+    }
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionMarkdown()
+    {
+        return $this->render('markdown');
+    }
+}
