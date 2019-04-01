ecf4ca9485436032b3c22857b39604e65eea5b80
myzero1 <myzero1@sina.com>
2019-04-01 20:16:50 +0800

    holdon

--- src/components/gii/generators/rest/form.php
+++ src/components/gii/generators/rest/form.php
@@ -5,13 +5,62 @@
 
 use kdn\yii2\JsonEditor;
 
+$onEditable = <<<'js'
+function onEditable(node) {
+    var unEditable = [
+        'swagger',
+        'info',
+        'host'
+    ];
+    if (Array.isArray(node.path)) {
+        var path = node.path.join('-');
+        if (unEditable.indexOf(path) > -1) {
+            return {
+              field: false,
+              value: true
+            };
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
+js;
+
+
+$generator->conf = json_encode([
+    'swagger' => '2.0',
+    'info' => 'info',
+    'host' => 'petstore.swagger.io',
+    'basePath' => '/v2',
+    'externalDocs' => [
+            'description' => 'Find out more about Swagger',
+            'url' => 'http://swagger.io',
+    ],
+    'schemes' => ['http', 'http'],
+    'securityDefinitions' => [
+        'api_key' => [
+                'type' => 'apiKey',
+                'name' => 'api_key',
+                'in' => 'header',
+        ],
+    ],
+    'paths' => 'paths',
+]);
+
+
+
 ?>
 <div class="rest-form">
 <?php
-    echo $form->field($generator, 'conf')->widget(
+    echo $form->field($generator, 'conf')->label('Api配置')->widget(
         '\kdn\yii2\JsonEditor',
         [
-            'clientOptions' => ['modes' => ['code', 'tree']],
+            'clientOptions' => [
+                'modes' => ['tree', 'view'],
+                'onEditable' => $onEditable,
+            ],
         ]
     );
 ?>
