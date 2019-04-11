96eb55200f759ac8261a6c0d93075c82f33535f1
myzero1 <myzero1@sina.com>
2019-04-11 13:54:15 +0800

    adjust the swagger.json

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace myzero1\restbyconf\controllers;
 
 use yii\web\Controller;
 use Yii;
+use myzero1\restbyconf\components\rest\Helper;
 
 /**
  * Default controller for the `test` module
@@ -34,8 +35,82 @@ class DefaultController extends Controller
      */
     public function actionSwaggerJson()
     {
-        $swaggerPath = Yii::getAlias('@vendor/myzero1/yii2-restbyconf/src/components/conf/swagger.json');
+        $swaggerPath = Yii::getAlias('@vendor/myzero1/yii2-restbyconf/src/components/conf/conf.json');
         $swaggerData = file_get_contents($swaggerPath);
+        $json = json_decode($swaggerData, true)['json'];
+        $oldTags = $json['tags'];
+        $tags = [];
+        $paths = [];
+
+        foreach ($oldTags as $k => $v) {
+            $tag = [];
+            $tag['name'] = $k;
+            $tag['description'] = $v['description'];
+            $tags[] = $tag;
+
+            $inputParams = [];
+            foreach ($v['paths'] as $k1 => $v1) {
+                $param = [];
+
+                foreach ($v1['inputs'] as $k2 => $v2) {
+                    $param[] = [
+                        'in' => $v2['type'],
+                        'name' => $v2,
+                        'description' => $v2['des'],
+                        'type' => 'string',
+                        'required' => $v2['required'],
+                        'example' => $v2['eg'],
+                    ];
+                }
+
+                $inputParams[$k1] = $param;
+            }
+
+            $path = [];
+            $pathData = [];
+            $pathOneData = [];
+            $pathPre = sprintf('/%s/', Helper::uncamelize($k,$separator='-'));
+            $pathOnePre = sprintf('%s/{id}/', $pathPre);
+            $pathData[$pathPre]['get'] = [
+                'tags' => $k,
+                'description' => $v['description'],
+                'operationId' => sprintf('%sController.Get All', $k),
+                'parameters' => $inputParams['index'],
+            ];
+            $pathData[$pathPre]['post'] = [
+                'tags' => $k,
+                'description' => $v['description'],
+                'operationId' => sprintf('%sController.Post', $k),
+                'parameters' => $inputParams['create'],
+            ];
+            $pathOneData[$pathOnePre]['get'] = [
+                'tags' => $k,
+                'description' => $v['description'],
+                'operationId' => sprintf('%sController.Get One', $k),
+                'parameters' => $inputParams['view'],
+            ];
+            $pathOneData[$pathOnePre]['put'] = [
+                'tags' => $k,
+                'description' => $v['description'],
+                'operationId' => sprintf('%sController.Put', $k),
+                'parameters' => $inputParams['update'],
+            ];
+            $pathOneData[$pathOnePre]['delete'] = [
+                'tags' => $k,
+                'description' => $v['description'],
+                'operationId' => sprintf('%sController.Delete', $k),
+                'parameters' => $inputParams['delete'],
+            ];
+
+            $paths['pathPre'] = $pathData[$pathPre];
+            $paths['pathOnePre'] = $pathData[$pathOnePre];
+        }
+
+
+        unset($json['tags']);
+        $json['tags'] = $tags;
+        $json['paths'] = $paths;
+        var_dump($json);exit;
 
         return $swaggerData;
     }
