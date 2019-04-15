b9ee247a3519b32720cdfd474721623cb90b16fc
myzero1 <myzero1@sina.com>
2019-04-15 23:11:30 +0800

    holdon

--- src/components/gii/generators/rest/form.php
+++ src/components/gii/generators/rest/form.php
@@ -4,8 +4,24 @@
 /* @var $generator yii\gii\generators\module\Generator */
 
 $asset = myzero1\restbyconf\assets\php\JsonEditorAsset::register($this);
-$confDataPath = Yii::getAlias('@vendor/myzero1/yii2-restbyconf/src/components/conf/conf.json');
-$confData = file_get_contents($confDataPath);
+$confDataPathTmp = Yii::getAlias('@vendor/myzero1/yii2-restbyconf/src/components/conf/conf.json');
+$confDataPathDefault = Yii::getAlias('@vendor/myzero1/yii2-restbyconf/src/components/conf/confDefault.json');
+
+if (is_file($confDataPathTmp)) {
+    $confDataTmp = file_get_contents($confDataPathTmp);
+    if (empty($confDataTmp)) {
+        $confDataInit = file_get_contents($confDataPathDefault);
+    } else {
+        $confDataInit = $confDataTmp;
+    }
+}
+
+if ($generator->conf) {
+    $confData = $generator->conf;
+} else {
+    $generator->conf = $confDataInit;
+    $confData = $confDataInit;
+}
 
 ?>
 
@@ -17,7 +33,8 @@ $confData = file_get_contents($confDataPath);
 
 <div class="rest-form">
     <?php
-        echo $form->field($generator, 'conf')->label('Api configuration')->hiddenInput();
+        echo $form->field($generator, 'conf')->label('Api configuration');
+        // echo $form->field($generator, 'conf')->label('Api configuration')->hiddenInput();
     ?>
 
     <div id="jsoneditor"></div>
