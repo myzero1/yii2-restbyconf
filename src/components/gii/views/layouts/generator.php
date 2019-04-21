0846ad4b1778c38c9f14394b2ddbd86171e98b11
myzero1 <myzero1@sina.com>
2019-04-21 23:47:04 +0800

    advance gii

--- src/components/gii/views/layouts/generator.php
+++ src/components/gii/views/layouts/generator.php
@@ -11,20 +11,7 @@ $activeGenerator = Yii::$app->controller->generator;
 ?>
 <?php $this->beginContent('@vendor/myzero1/yii2-restbyconf/src/components/gii/views/layouts/main.php'); ?>
 <div class="row">
-    <div class="col-md-3 col-sm-4">
-        <div class="list-group">
-            <?php
-            $classes = ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-center'];
-            foreach ($generators as $id => $generator) {
-                $label = Html::tag('span', Html::encode($generator->getName())) . '<span class="icon"></span>';
-                echo Html::a($label, ['default/view', 'id' => $id], [
-                    'class' => $generator === $activeGenerator ? array_merge($classes, ['active']) : $classes,
-                ]);
-            }
-            ?>
-        </div>
-    </div>
-    <div class="col-md-9 col-sm-8">
+    <div class="col-lg-12 col-md-12 col-sm-12">
         <?= $content ?>
     </div>
 </div>
