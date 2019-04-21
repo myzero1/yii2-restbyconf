b1eabbf1fef6d6e59bab2304918aff05157eb256
myzero1 <myzero1@sina.com>
2019-04-22 00:42:06 +0800

    adjust the gii theme

--- src/components/gii/views/layouts/generator.php
+++ src/components/gii/views/layouts/generator.php
@@ -12,7 +12,32 @@ $activeGenerator = Yii::$app->controller->generator;
 <?php $this->beginContent('@vendor/myzero1/yii2-restbyconf/src/components/gii/views/layouts/main.php'); ?>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
+        <br>
+        <div class="list-group">
+            <?php
+            $classes = ['list-group-item', 'd-flex', 'justify-content-between', 'align-items-center'];
+            foreach ($generators as $id => $generator) {
+                $label = Html::tag('span', Html::encode(str_replace(' Generator', '', $generator->getName()))) . '<span class="icon"></span>';
+                echo Html::a($label, ['default/view', 'id' => $id], [
+                    'class' => $generator === $activeGenerator ? array_merge($classes, ['active']) : $classes,
+                ]);
+            }
+            ?>
+        </div>
+
         <?= $content ?>
     </div>
 </div>
+
+<style type="text/css">
+    .list-group{
+        overflow: hidden;
+    }
+    .list-group-item{
+        float: left;
+    }
+    .list-group-item{
+        border-radius: 0 !important;
+    }
+</style>
 <?php $this->endContent(); ?>
