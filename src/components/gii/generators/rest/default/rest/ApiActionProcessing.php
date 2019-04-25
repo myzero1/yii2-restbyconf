463799648bbdcdc045eac3c42a9d0751e34f351e
myzero1 <myzero1@sina.com>
2019-04-25 13:06:56 +0800

    holdon

--- src/components/gii/generators/rest/default/rest/ApiActionProcessing.php
+++ src/components/gii/generators/rest/default/rest/ApiActionProcessing.php
@@ -52,12 +52,12 @@ $egOutputData = $outputs;
 echo "<?php\n";
 ?>
 /**
-* @link https://github.com/myzero1
-* @copyright Copyright (c) 2019- My zero one
-* @license https://github.com/myzero1/yii2-restbyconf/blob/master/LICENSE
-*/
+ * @link https://github.com/myzero1
+ * @copyright Copyright (c) 2019- My zero one
+ * @license https://github.com/myzero1/yii2-restbyconf/blob/master/LICENSE
+ */
 
-namespace <?= $processingClassNs ?>;
+namespace <?=$processingClassNs?>;
 
 use Yii;
 use yii\base\DynamicModel;
@@ -67,211 +67,210 @@ use myzero1\restbyconf\components\rest\ApiCodeMsg;
 use myzero1\restbyconf\components\rest\ApiActionProcessing;
 
 /**
-* implement the UpdateProcessing
-*
-* For more details and usage information on CreateAction, see the [guide article](https://github.com/myzero1/yii2-restbyconf).
-*
-* @author Myzero1
-<myzero1@sina.com>
-* @since 0.0
-*/
-class <?= $actionClass ?> implements ApiActionProcessing
+ * implement the UpdateProcessing
+ *
+ * For more details and usage information on CreateAction, see the [guide article](https://github.com/myzero1/yii2-restbyconf).
+ *
+ * @author Myzero1 <myzero1@sina.com>
+ * @since 0.0
+ */
+class <?=$actionClass?> implements ApiActionProcessing
 {
-/**
-* @param $id mixed
-* @return array date will return to create action.
-* @throws ServerErrorHttpException
-* @throws \yii\base\InvalidConfigException
-*/
-public function processing($id=null)
-{
-// the path and query params will geted by queryParams,and the path params will rewrite the query params.
-$input['get'] = Yii::$app->request->queryParams;
-$input['post'] = Yii::$app->request->bodyParams;
-$validatedInput = $this->inputValidate($input);
-if (Helper::isReturning($validatedInput)) {
-return $validatedInput;
-} else {
-// $in2dbData = $this->mappingInput2db($validatedInput);
-// $completedData = $this->completeData($in2dbData);
-// $handledData = $this->handling($completedData);
-// $db2outData = $this->mappingDb2output($handledData);
-$db2outData = $this->egOutputData();// for demo
-$result = $this->completeResult($db2outData);
-return $result;
-}
-}
+    /**
+     * @param $id mixed
+     * @return array date will return to create action.
+     * @throws ServerErrorHttpException
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function processing($id=null)
+    {
+        // the path and query params will geted by queryParams,and the path params will rewrite the query params.
+        $input['get'] = Yii::$app->request->queryParams;
+        $input['post'] = Yii::$app->request->bodyParams;
+        $validatedInput = $this->inputValidate($input);
+        if (Helper::isReturning($validatedInput)) {
+            return $validatedInput;
+        } else {
+            // $in2dbData = $this->mappingInput2db($validatedInput);
+            // $completedData = $this->completeData($in2dbData);
+            // $handledData = $this->handling($completedData);
+            // $db2outData = $this->mappingDb2output($handledData);
+            $db2outData = $this->egOutputData();// for demo
+            $result = $this->completeResult($db2outData);
+            return $result;
+        }
+    }
 
-/**
-* @param  array $input from the request body
-* @return array
-*/
-public function inputValidate($input)
-{
-// get
-$modelGet = new DynamicModel([
+    /**
+     * @param  array $input from the request body
+     * @return array
+     */
+    public function inputValidate($input)
+    {
+        // get
+        $modelGet = new DynamicModel([
 <?php foreach ($getInputsKeys as $key => $value) { ?>
-    '<?= $value ?>',
+            '<?=$value?>',
 <?php } ?>
-]);
+        ]);
 
 <?php foreach ($getInputRules as $key => $value) { ?>
-    <?= $value . "\n" ?>
+        <?=$value."\n"?>
 <?php } ?>
 
-$modelGet->load($input['get'], '');
+        $modelGet->load($input['get'], '');
 
-if (!$modelGet->validate()) {
-$errors = $modelGet->errors;
-return [
-'code' => ApiCodeMsg::CLIENT_ERROR,
-'msg' => Helper::getErrorMsg($errors),
-'data' => $errors,
-];
-}
+        if (!$modelGet->validate()) {
+            $errors = $modelGet->errors;
+            return [
+                'code' => ApiCodeMsg::CLIENT_ERROR,
+                'msg' => Helper::getErrorMsg($errors),
+                'data' => $errors,
+            ];
+        }
 
-// post
-$modelPost = new DynamicModel([
+        // post
+        $modelPost = new DynamicModel([
 <?php foreach ($postInputsKeys as $key => $value) { ?>
-    '<?= $value ?>',
+            '<?=$value?>',
 <?php } ?>
-]);
+        ]);
 
 <?php foreach ($postInputRules as $key => $value) { ?>
-    <?= $value . "\n" ?>
+        <?=$value."\n"?>
 <?php } ?>
 
-$modelPost->load($input['post'], '');
+        $modelPost->load($input['post'], '');
 
-if (!$modelPost->validate()) {
-$errors = $modelPost->errors;
-return [
-'code' => ApiCodeMsg::CLIENT_ERROR,
-'msg' => Helper::getErrorMsg($errors),
-'data' => $errors,
-];
-}
+        if (!$modelPost->validate()) {
+            $errors = $modelPost->errors;
+            return [
+                'code' => ApiCodeMsg::CLIENT_ERROR,
+                'msg' => Helper::getErrorMsg($errors),
+                'data' => $errors,
+            ];
+        }
 
-return array_merge($modelGet->attributes, $modelPost->attributes);
-}
+        return array_merge($modelGet->attributes, $modelPost->attributes);
+    }
 
-/**
-* @param  array $validatedInput validated data
-* @return array
-*/
-public function mappingInput2db($validatedInput)
-{
-$inputFieldMap = [
-'demo_name' => 'name',
-'demo_description' => 'description',
-];
-$in2dbData = Helper::input2DbField($validatedInput, $inputFieldMap);
+    /**
+     * @param  array $validatedInput validated data
+     * @return array
+     */
+    public function mappingInput2db($validatedInput)
+    {
+        $inputFieldMap = [
+            'demo_name' => 'name',
+            'demo_description' => 'description',
+        ];
+        $in2dbData = Helper::input2DbField($validatedInput, $inputFieldMap);
 
-return $in2dbData;
-}
+        return $in2dbData;
+    }
 
-/**
-* @param  array $in2dbData mapped data form input
-* @return array
-*/
-public function completeData($in2dbData)
-{
-$time = time();
-$in2dbData['updated_at'] = $time;
+    /**
+     * @param  array $in2dbData mapped data form input
+     * @return array
+     */
+    public function completeData($in2dbData)
+    {
+        $time = time();
+        $in2dbData['updated_at'] = $time;
 
-return $in2dbData;
-}
+        return $in2dbData;
+    }
 
-/**
-* @param  array $completedData completed data
-* @return array
-* @throws ServerErrorHttpException
-*/
-public function handling($completedData)
-{
+    /**
+     * @param  array $completedData completed data
+     * @return array
+     * @throws ServerErrorHttpException
+     */
+    public function handling($completedData)
+    {
 
-}
+    }
 
-/**
-* @param  array $savedData saved data
-* @return array
-*/
-public function mappingDb2output($handledData)
-{
-$outputFieldMap = [
-'name' => 'demo_name',
-'description' => 'demo_description',
-];
-$db2outData = Helper::db2OutputField($handledData, $outputFieldMap);
+    /**
+     * @param  array $savedData saved data
+     * @return array
+     */
+    public function mappingDb2output($handledData)
+    {
+        $outputFieldMap = [
+            'name' => 'demo_name',
+            'description' => 'demo_description',
+        ];
+        $db2outData = Helper::db2OutputField($handledData, $outputFieldMap);
 
-$db2outData['created_at'] = Helper::time2string($db2outData['created_at']);
-$db2outData['updated_at'] = Helper::time2string($db2outData['updated_at']);
+        $db2outData['created_at'] = Helper::time2string($db2outData['created_at']);
+        $db2outData['updated_at'] = Helper::time2string($db2outData['updated_at']);
 
-return $db2outData;
-}
+        return $db2outData;
+    }
 
-/**
-* @param  array $db2outData completed data form database
-* @param  array $extra
-* @return array
-*/
-public function completeResult($db2outData=[], $extra = [])
-{
-$result = [
-'code' => ApiCodeMsg::SUCCESS,
-'msg' => ApiCodeMsg::SUCCESS_MSG,
-'data' => $db2outData,
-'extra' => $extra,
-];
+    /**
+     * @param  array $db2outData completed data form database
+     * @param  array $extra
+     * @return array
+     */
+    public function completeResult($db2outData=[], $extra = [])
+    {
+        $result = [
+            'code' => ApiCodeMsg::SUCCESS,
+            'msg' => ApiCodeMsg::SUCCESS_MSG,
+            'data' => $db2outData,
+            'extra' => $extra,
+        ];
 
-return $result;
-}
+        return $result;
+    }
 
-/**
-* @param  array $db2outData completed data form database
-* @param  array $extra
-* @return array
-*/
-public function getSort($validatedInput, $fields, $defafult)
-{
-if (isset($validatedInput['sort'])) {
-$sortInfo = Helper::getSort($validatedInput['sort'], $fields, $defafult);
-} else {
-$sortInfo = Helper::getSort('+myzeroqtest', $fields, $defafult);
-}
+    /**
+     * @param  array $db2outData completed data form database
+     * @param  array $extra
+     * @return array
+     */
+    public function getSort($validatedInput, $fields, $defafult)
+    {
+        if (isset($validatedInput['sort'])) {
+            $sortInfo = Helper::getSort($validatedInput['sort'], $fields, $defafult);
+        } else {
+            $sortInfo = Helper::getSort('+myzeroqtest', $fields, $defafult);
+        }
 
-return $sortInfo;
-}
+        return $sortInfo;
+    }
 
-/**
-* @param  array $db2outData completed data form database
-* @param  array $extra
-* @return array
-*/
-public function getPagination($validatedInput)
-{
-$pagination = [];
-if (isset($validatedInput['page'])) {
-$validatedInput['page'] = $validatedInput['page'];
-} else {
-$pagination['page'] = 1;
-}
-if (isset($validatedInput['page_size'])) {
-$pagination['page_size'] = $validatedInput['page_size'];
-} else {
-$pagination['page_size'] = 30;
-}
+    /**
+     * @param  array $db2outData completed data form database
+     * @param  array $extra
+     * @return array
+     */
+    public function getPagination($validatedInput)
+    {
+        $pagination = [];
+        if (isset($validatedInput['page'])) {
+            $validatedInput['page'] = $validatedInput['page'];
+        } else {
+            $pagination['page'] = 1;
+        }
+        if (isset($validatedInput['page_size'])) {
+            $pagination['page_size'] = $validatedInput['page_size'];
+        } else {
+            $pagination['page_size'] = 30;
+        }
 
-return $pagination;
-}
+        return $pagination;
+    }
 
-/**
-* @return array
-*/
-public function egOutputData()
-{
-$egOutputData = '<?= serialize($egOutputData) ?>';
+    /**
+     * @return array
+     */
+    public function egOutputData()
+    {
+        $egOutputData = '<?=serialize($egOutputData)?>';
 
-return unserialize($egOutputData);
-}
+        return unserialize($egOutputData);
+    }
 }
