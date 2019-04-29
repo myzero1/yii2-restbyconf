cfc7be64b2a993debab773df0cf7435240545ab0
myzero1 <myzero1@sina.com>
2019-04-29 11:20:37 +0800

    fix

--- src/components/conf/apiUrlRules.php
+++ src/components/conf/apiUrlRules.php
@@ -1,11 +1,12 @@
 <?php
 return [
-    '/v2/controller' => [
-        'controller' => ['/v2/controller'],
+    'v1/demo' => [
+        'controller' => ['v1/demo'],
         'class' => '\yii\rest\UrlRule',
         'pluralize' => false,
         'extraPatterns' => [
-            'POST,OPTIONS /<in_str:^\w{1,32}$>/action' => 'action',
+            'GET,OPTIONS /export' => 'export',
+            'PATCH,OPTIONS <id:\d{1,32}>/custom' => 'custom',
         ],
     ],
 ];
