3a2e56ef9e802a46e63922c9471ae84a0e8c50ad
myzero1 <myzero1@sina.com>
2019-04-30 10:23:15 +0800

    fix markdown

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -226,6 +226,7 @@ class DefaultController extends Controller
             $content .= sprintf("\n<a name='1.2.%s' ></a> \n", $i);
             $content .= sprintf("### 1.2.%s. %s \n", $i, $oldcontroller);
             $content .= sprintf("> [Go table](#1.1) \n\n");
+            $content .= sprintf("&nbsp;`description` \n\n");
             $content .= sprintf("%s \n", $oldcontrollerV['description']);
 
             $j = 0;
@@ -236,7 +237,11 @@ class DefaultController extends Controller
                 $content .= sprintf("\n<a name='1.2.%s.%s' ></a> \n", $i, $j);
                 $content .= sprintf("#### 1.2.%s.%s. %s \n", $i, $j, $action);
                 $content .= sprintf("> [Go table](#1.1) \n\n");
-                $content .= sprintf("&nbsp;`Basic info` \n\n");
+                $content .= sprintf("&nbsp;`summary` \n\n");
+                $content .= sprintf("%s \n\n", $actionsV['summary']);
+                $content .= sprintf("&nbsp;`description` \n\n");
+                $content .= sprintf("%s \n", $actionsV['description']);
+                $content .= sprintf("\n&nbsp;`Basic info` \n\n");
                 $content .= sprintf("| Items | Detail | \n");
                 $content .= sprintf("|-------|:---------:| \n");
                 $content .= sprintf("| Des | %s | \n", $actionsV['description']);
