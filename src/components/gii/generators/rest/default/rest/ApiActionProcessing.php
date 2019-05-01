f2a2dd99f6cdb7bfcc8109f1437d822f116241ca
myzero1 <myzero1@sina.com>
2019-05-02 07:23:56 +0800

    holdon

--- src/components/gii/generators/rest/default/rest/ApiActionProcessing.php
+++ src/components/gii/generators/rest/default/rest/ApiActionProcessing.php
@@ -152,8 +152,7 @@ class <?=$actionClass?> implements ApiActionProcessing
      */
     public function completeData($in2dbData)
     {
-        $time = time();
-        $in2dbData['updated_at'] = $time;
+        $in2dbData['created_at'] = $in2dbData['updated_at'] = time();
 
         $in2dbData = ApiHelper::inputFilter($in2dbData);
 
