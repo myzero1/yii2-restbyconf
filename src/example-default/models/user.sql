ff7c35e8b4d1d105e3cecd21b9c0991befd04c4c
myzero1 <myzero1@sina.cn>
2019-07-08 12:57:21 +0800

    add defult record

--- src/example-default/models/user.sql
+++ src/example-default/models/user.sql
@@ -13,6 +13,10 @@ File Encoding         : 65001
 Date: 2019-05-09 18:55:10
 */
 
+/*
+Add default data for user
+*/
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -30,4 +34,9 @@ CREATE TABLE `user` (
   `updated_at` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'api', '$2y$13$DvDUk.R6GdniDKQuCe8hdeR6nSqusRf4yc0cOOKmCqcKMbgyrn7Oq', '5T4Fr4xcwk15RwyzBdFuT7ngFCYarwt-_1557489871', '0', '1', '1552886962', '1552886962');
