8dfd471fa001ac0a1be2d7ca4a8ef7597607223d
myzero1 <myzero1@sina.cn>
2019-07-09 14:18:53 +0800

    Update markdown.php
    add sidebar for markdown

--- src/views/default/markdown.php
+++ src/views/default/markdown.php
@@ -3,5 +3,37 @@
 	\yii\bootstrap\BootstrapAsset::register($this);
 ?>
 <div class="container">
-	<?= $markdownHtml?>
+	<div id="doc-sidebar"></div>
+	<div id="doc-body"><?= $markdownHtml?></div>
 </div>
+
+
+<style type="text/css">
+	#doc-sidebar{
+	    width: 200px;
+	    border-right: 2px solid #aaa;
+	    height: 100vh;
+	    position: fixed;
+	    overflow: auto;
+	    background: #fff;
+    	padding-left: 10px;
+	}
+	#doc-body{
+		padding-left: 220px;
+	}
+	
+</style>
+
+<?php
+$js = <<<'JS'
+	var sidebarObj = $('#doc-body').children('h2').first();
+	sidebar = sidebarObj.html() + sidebarObj.next().html();
+
+	$('#doc-sidebar').html(sidebar);
+
+	console.log(sidebar);
+JS;
+
+$this->registerJs($js);
+
+?>
