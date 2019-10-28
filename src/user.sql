20254e4794a094d6f35e1be075e62e45ca3acb96
myzero1 <myzero1@sina.com>
2019-10-28 23:15:11 +0800

    holdon

--- src/user.sql
+++ src/user.sql
@@ -22,6 +22,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `api_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `is_del` tinyint(4) NOT NULL DEFAULT 0,
@@ -35,4 +36,4 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '$2y$13$OI.DKfRnS3JWVn0oEiHRe.zTzuFali/ZJ5PAD8.dYtWyUpWOA.Mfq', '123456', '0', '1', '1551621771', '1551621771');
+INSERT INTO `user` VALUES ('1', 'admin', 'avatar.png', '$2y$13$OI.DKfRnS3JWVn0oEiHRe.zTzuFali/ZJ5PAD8.dYtWyUpWOA.Mfq', '123456', '0', '1', '1551621771', '1551621771');
