e7000c28a799e7d7bce71f8c7de641fcb2ba95be
myzero1 <myzero1@sina.com>
2019-10-31 09:44:05 +0800

    add ignore for images

--- src/components/gii/generators/rest/default/rest/template/ApiUploadProcessing.php
+++ src/components/gii/generators/rest/default/rest/template/ApiUploadProcessing.php
@@ -124,7 +124,11 @@ class <?= $templateParams['className'] ?> implements ApiActionProcessing
                     ]
                 ],
             ];
-        }           
+        }
+
+        $ignore = sprintf('%s/.gitignore', $newDirectory);
+        file_put_contents ( $ignore , "*\n!.gitignore" );
+              
         $file->saveAs($newName);
 
         $host = 'http://<?= $templateParams['conf']['json']['host']?>';
