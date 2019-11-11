dce2d25a72a00c028aceeb4b2d82180eb0726013
myzero1 <myzero1@sina.com>
2019-11-11 14:57:22 +0800

    ajsut result

--- src/components/gii/generators/rest/default/rest/template/ApiCaptchaProcessing.php
+++ src/components/gii/generators/rest/default/rest/template/ApiCaptchaProcessing.php
@@ -44,7 +44,7 @@ class <?= $templateParams['className'] ?> implements ApiActionProcessing
         $input['post'] = Yii::$app->request->bodyParams;
         $validatedInput = $this->inputValidate($input);
         if (Helper::isReturning($validatedInput)) {
-            return $validatedInput;
+            return Helper::wrapReturn($validatedInput);
         } else {
             $in2dbData = $this->mappingInput2db($validatedInput);
             $completedData = $this->completeData($in2dbData);
@@ -54,7 +54,7 @@ class <?= $templateParams['className'] ?> implements ApiActionProcessing
             $handledData = HandlingHelper::after($handledData);
 
             if (Helper::isReturning($handledData)) {
-                return $handledData;
+                return Helper::wrapReturn($handledData);
             }
 
             $db2outData = $this->mappingDb2output($handledData);
@@ -148,25 +148,13 @@ class <?= $templateParams['className'] ?> implements ApiActionProcessing
      */
     public function completeResult($db2outData = [])
     {
-        if ( isset($db2outData['response_code']) ) {
-            $responseCode = $db2outData['response_code'];
-            unset($db2outData['response_code']);
-        } else {
-            $responseCode = 735200;
-        }
-
-        if ( isset($db2outData['response_msg']) ) {
-            $responseMsg = $db2outData['response_msg'];
-            unset($db2outData['response_msg']);
-        } else {
-            $responseMsg = ApiCodeMsg::SUCCESS_MSG;
-        }
-        
         $result = [
             'code' => $responseCode,
             'msg' => $responseMsg,
-            'data' => is_null($db2outData) ? new \stdClass() : $db2outData,
+            'data' => $db2outData,
         ];
+
+        $result = Helper::wrapReturn($result);
 
         return $result;
     }
